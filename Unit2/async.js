2582ac5a1f22c4ed8ac160363e139dac2b55cf2a
iDrqzz <156222966+iDrqzz@users.noreply.github.com>
2024-03-17 19:16:00 -0500

    async

---
+++ Unit2/async.js
@@ -0,0 +1,30 @@
+async function bucksRace() {
+
+    let runner1 = false;
+    let runner2 = false;
+    let loser = '';
+
+    const runner1Go = new Promise((resolve) => {
+        setTimeout(() => {
+            loser = 'runner1';
+            resolve(true);
+        }, Math.random() * 10000);
+    });
+
+    const runner2Go = new Promise((resolve) => {
+        setTimeout(() => {
+            loser = 'runner2';
+            resolve(true);
+        }, Math.random() * 10000);
+    });
+
+ 
+    const results = await Promise.all([runner1Go, runner2Go]);
+
+
+    return [results[0], results[1], loser];
+}
+
+bucksRace().then((result) => {
+    console.log("The loser is:", result[2]);
+});
